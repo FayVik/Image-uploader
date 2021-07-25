cf5eae03153bdd577667ffdbc6b4551a2adf8ae2
FayVik <vivianemma97@gmail.com>
2021-07-25 23:31:20 +0100

    remove key

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from 'firebase/app';
 import 'firebase/storage';
 
 const firebaseConfig = {
-	apiKey: 'AIzaSyB-vwbOUWeJxqfYBsBoxIpIljlxj5SQEUU',
-	authDomain: 'uploader-89d2b.firebaseapp.com',
-	projectId: 'uploader-89d2b',
-	storageBucket: 'uploader-89d2b.appspot.com',
-	messagingSenderId: '743786265718',
-	appId: '1:743786265718:web:a9f465eff174b5941f3357',
-	measurementId: 'G-9CSF20TBR6',
+	apiKey: '**********************',
+	authDomain: '*******************',
+	projectId: '**********************',
+	storageBucket: '**********************',
+	messagingSenderId: '**********************',
+	appId: '**********************',
+	measurementId: '**********************',
 };
 
 firebase.initializeApp(firebaseConfig);
